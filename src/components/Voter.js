2da20ba0cd3816575f41f12814ebbba848dc16b2
John Nghiem <johnnghiem@Johns-MacBook-Pro.local>
2019-07-22 12:16:39 +0100

    styling on voting buttons

--- src/components/Voter.js
+++ src/components/Voter.js
@@ -13,13 +13,13 @@ class Voter extends React.Component {
             <div className={styles.voter}>
                 <button className={this.state.voteMod === 1 ? styles.disable_up : styles.enable_up} 
                 onClick={()=> this.voteAdder(1)} 
-                disabled={this.state.voteMod === 1}><span className={styles.emojiUp}>👍</span></button>
+                disabled={this.state.voteMod === 1}>👍</button>
                 
                 <h3>Votes: {this.props.votes + this.state.voteMod}</h3>
                 
                 <button className={this.state.voteMod === -1 ? styles.disable_down : styles.enable_down} 
                 onClick={()=> this.voteAdder(-1)} 
-                disabled={this.state.voteMod === -1} ><span className={styles.emojiDown}>👎</span></button>
+                disabled={this.state.voteMod === -1} >👎</button>
             </div>
         )
     }
